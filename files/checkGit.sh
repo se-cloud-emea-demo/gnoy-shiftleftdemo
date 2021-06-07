620e9f17cb433dcebd64f8d04acb9c379a07b22b
GuyNoy <62098145+GuyNoy@users.noreply.github.com>
2021-06-07 06:17:44 +0300

    Update checkGit.sh

--- files/checkGit.sh
+++ files/checkGit.sh
@@ -5,7 +5,7 @@ sleep 5
 #replace <buildname> with your buildname of your demo-build.yml file.
 REPOOUTPUT=$(curl -k -u $TL_USER:$TL_PASS \
   -H 'Content-Type: application/json' \
-  https://$TL_CONSOLE/api/v1/coderepos?id=se-cloud-emea-demo%2Fgnoy-evil.petclinic&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
+  https://console-master-guynoy.guynoy.demo.twistlock.com/api/v1/coderepos?id=se-cloud-emea-demo%2Fgnoy-evil.petclinic&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
  
 
 #sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d: > output.txt
